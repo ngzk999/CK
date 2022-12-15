e7edaa754b5ed7404b240aa455dfbc59e715caf3
SDPzk <n.zheng.keong@simedarbyplantation.com>
2022-12-15 22:46:27 +0800

    Welcome Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,26 @@
+import 'package:ck/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CK());
 }
+
+class CK extends StatelessWidget {
+  const CK({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        dividerTheme: const DividerThemeData(
+          color: Colors.grey
+        ),
+        textTheme: const TextTheme(
+          bodyText1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: const WelcomeScreen(),
+    );
+  }
+}
+
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              children: [
+                SizedBox(
+                  height: 60.0,
+                  child: Image.asset('images/ck_logo.png'),
+                ),
+                const Text(
+                  'CK Mart',
+                  style: TextStyle(
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 48.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                elevation: 5.0,
+                color: Colors.lightBlueAccent,
+                borderRadius: BorderRadius.circular(30.0),
+                child: MaterialButton(
+                  onPressed: () {
+                    // Go to login screen
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: const Text('Log in'),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                elevation: 5.0,
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.circular(30.0),
+                child: MaterialButton(
+                  onPressed: () {
+                    // Go to register screen
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: const Text('Register'),
+                ),
+              ),
+            ),
+            const Divider(),
+            // Padding(
+            //   padding: const EdgeInsets.symmetric(vertical: 16.0),
+            //   child: SizedBox(
+            //     width: 200.0,
+            //     height: 42.0,
+            //     child: SignInButton(
+            //       Buttons.googleDark,
+            //       elevation: 5.0,
+            //       onPressed: () {
+            //         AuthService().signInWithGoogle();
+            //       },
+            //     ),
+            //   )
+            // )
+          ],
+        ),
+      ),
+    );
+  }
+}
